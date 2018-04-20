0d8ac5fe21f58c05d46118bc38fbf20660111f61
HughWen <ruoningwen@gmail.com>
2018-04-20 19:07:26 +0800

    fix the code style

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 import influxdb
 import os
 
+
 def make_datas():
     datapoints = []
     for a in api.attractions():
@@ -40,15 +41,18 @@ def crawl_one():
     datapoints = make_datas()
     assert db.write_points(datapoints, database='disney', batch_size=50)
 
+
 def crawl_loop():
     db = get_influxdb()
     while True:
         hour = datetime.datetime.now().hour
         if hour >= 7 and hour <= 20:
             datapoints = make_datas()
-            assert db.write_points(datapoints, database='disney', batch_size=50)
+            assert db.write_points(
+                datapoints, database='disney', batch_size=50)
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         time.sleep(30)
+
 
 if __name__ == '__main__':
     crawl_loop()
